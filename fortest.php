537d20d4ebb4cac4f4f0f4dec774cb5f896c4cce
lnroma <family_89@mail.ru>
2016-03-10 21:43:33 +0300

    Update fortest.php

--- fortest.php
+++ fortest.php
@@ -10,5 +10,12 @@
           ]
  ];
  
-echo isset($c[0][0][key]) ? 'testing' : 'testing3';
-if(isset$)
+function user_f($c) { 
+  $result = isset($c[0][0]['key']) ? true : false;
+  if(isset($c[0][0]['key2'])) 
+//     return $result;
+  return false;
+}
+ 
+var_dump(user_f($c)); 
+ 
